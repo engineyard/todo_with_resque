60a8e9f0210b6d9fa550087ce9757e9f79715c7a
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-15 21:21:01 -0700

    main/recipe - modify for chef 0.6 support

--- deploy/cookbooks/main/attributes/recipe.rb
+++ deploy/cookbooks/main/attributes/recipe.rb
@@ -1,3 +1,7 @@
-recipes('main')
-owner_name(@attribute[:users].first[:username])
-owner_pass(@attribute[:users].first[:password])
+if Chef::VERSION == '0.6.0.2'
+  recipes("main")
+end
+
+# TODO restore these
+# owner_name(@attribute[:users].first[:username])
+# owner_pass(@attribute[:users].first[:password])
