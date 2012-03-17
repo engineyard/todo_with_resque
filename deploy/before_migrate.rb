f5420cf4f7b7ba100d1a4532539fa17eca9e659c
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-16 19:47:41 -0700

    bump resque recipe; print nice message if no chef errors

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -11,4 +11,4 @@ end
 # Runs application cookbooks
 run "cd #{latest_release}; sudo bundle exec chef-solo -c #{latest_release}/deploy/solo.rb -j /etc/chef-custom/dna.json"
 
-run "cat #{latest_release}/deploy/chef-stacktrace.out"
+run "cat #{latest_release}/deploy/chef-stacktrace.out 2> /dev/null || echo 'Chef run successful'"
