5a8823dcaa2696ef263e76ade55bae80be14e276
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-17 22:37:39 -0700

    reverted the resque/monit group name to what it already was (and I misunderstood)

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,5 +1,5 @@
 on_utilities(:resque) do
   node[:applications].each do |app_name, data|
-    sudo 'echo "sleep 20 && monit -g resque_#{app_name} restart all" | at now'
+    sudo 'echo "sleep 20 && monit -g #{app_name}_resque restart all" | at now'
   end
 end
