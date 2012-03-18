87d9b89183f249d7a3193135990191920a86922f
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-17 23:22:18 -0700

    clean up main recipe

--- deploy/cookbooks/main/attributes/recipe.rb
+++ deploy/cookbooks/main/attributes/recipe.rb
@@ -1,7 +1,3 @@
 if Chef::VERSION == '0.6.0.2'
   recipes("main")
 end
-
-# TODO restore these
-# owner_name(@attribute[:users].first[:username])
-# owner_pass(@attribute[:users].first[:password])
--- deploy/cookbooks/main/recipes/default.rb
+++ deploy/cookbooks/main/recipes/default.rb
@@ -1,3 +1,2 @@
-
 require_recipe 'redis'
 require_recipe 'resque'
